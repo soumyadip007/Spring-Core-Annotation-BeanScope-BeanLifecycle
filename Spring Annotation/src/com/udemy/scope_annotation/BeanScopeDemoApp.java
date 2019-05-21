36001793343009710c041bf98c535a0dc0c4f8b9
soumyadip007 <soumyadip.note@gmail.com>
2019-05-21 18:00:33 +0530

    BeanScope Singleton

--- Spring Annotation/src/com/udemy/scope_annotation/BeanScopeDemoApp.java
+++ Spring Annotation/src/com/udemy/scope_annotation/BeanScopeDemoApp.java
@@ -1,5 +1,48 @@
 package com.udemy.scope_annotation;
 
-public class BeanScopeDemoApp {
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class BeanScopeDemoApp {
+	
+public static void main(String[] args) {
+		
+		//read Spring Config
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		
+		
+		//Get the bean
+		Coach obj = context.getBean("scopePrototype", Coach.class);
+
+		Coach obj1 = context.getBean("scopePrototype", Coach.class);
+		
+		
+		//Check result
+		boolean result= (obj==obj1);
+		
+		System.out.println("\nBoth objects are same = "+result);
+		
+		System.out.println("\nMemory alocation for obj is = "+obj);
+
+		System.out.println("\nMemory alocation for obj1 is = "+obj1);
+				
+				
+		//Close the context
+				
+		context.close();
+	}
 }
+
+/*
+
+Output  (Singleton or Default):
+
+Both objects are same = true
+
+Memory alocation for obj is = com.udemy.scope_annotation.CricketCoach@71a794e5
+
+Memory alocation for obj1 is = com.udemy.scope_annotation.CricketCoach@71a794e5
+
+
+*/
--- Spring Annotation/src/com/udemy/scope_annotation/CricketCoach.java
+++ Spring Annotation/src/com/udemy/scope_annotation/CricketCoach.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
-@Component("ScopePrototype")
+@Component("scopePrototype")
 public class CricketCoach implements Coach {
 
 
