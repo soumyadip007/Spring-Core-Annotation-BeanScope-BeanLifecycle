8ca718dd09ba73ba8790be3f687dafccdcbce3f2
soumyadip007 <soumyadip.note@gmail.com>
2019-05-21 13:47:41 +0530

    Annotation Engine added (Main File)

--- Spring Annotation/src/com/udemy/springannotation/AnnotationDemoApp.java
+++ Spring Annotation/src/com/udemy/springannotation/AnnotationDemoApp.java
@@ -1,10 +1,26 @@
 package com.udemy.springannotation;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AnnotationDemoApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//read Spring Config
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
 
+		
+		
+		//Get the bean
+		Coach obj = context.getBean("thatSillyCoach", Coach.class);
+		
+		//Call Method on the bean
+		System.out.println(obj.getDailyWorkout());
+		
+		
+		//Close the context
+		context.close();
 	}
 
 }
