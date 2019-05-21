1b872cec35dc2918a4d88bf205a8e2fa54fe7ede
soumyadip007 <soumyadip.note@gmail.com>
2019-05-21 15:47:06 +0530

    New Componets Dtabase For qualif.

---
+++ Spring Annotation/src/com/udemy/field_injection/DatabaseFortuneService.java
@@ -0,0 +1,17 @@
+package com.udemy.field_injection;
+
+public class DatabaseFortuneService implements FortuneService {
+
+	@Override
+	public String getFortuneService() {
+		// TODO Auto-generated method stub
+		return "Autowiring Applied with field injection //DatabaseFortuneService";
+	}
+
+	@Override
+	public void methodInjection() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
