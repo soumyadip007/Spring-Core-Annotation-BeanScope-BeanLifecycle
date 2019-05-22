0dae3953a589b5b662bdd8a7dbe8c7ec56707dd2
soumyadip007 <soumyadip.note@gmail.com>
2019-05-22 13:26:23 +0530

    Configuring Properties

--- Spring Annotation/src/com/udemy/springConfigWithoutXml/SpringConfig.java
+++ Spring Annotation/src/com/udemy/springConfigWithoutXml/SpringConfig.java
@@ -3,9 +3,11 @@ package com.udemy.springConfigWithoutXml;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("com.udemy.springConfigWithoutXml")
+@PropertySource("Classpath.sport.properties")
 public class SpringConfig {
 
 	
